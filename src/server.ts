c97cf6bb375a56000339bef57dae20fd309b86fc
Gérondal Fahran <fahran.grdl@gmail.com>
2025-07-22 10:40:13 +0200

    Correction commentaire server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
-
-
 const app = express();
 const port = 4545;
 
@@ -13,22 +11,13 @@ const __dirname = path.dirname(__filename);
 
 // Configuration du moteur de template EJS
 
-
-//Utilité : Permet d’utiliser les layouts (<%- body %>)
-//  et les partials facilement dans EJS.
-
 app.set('view engine','ejs');
 //But : Définit EJS comme moteur de templates par défaut.
-//Utilité : Tu peux utiliser res.render('page') pour afficher une vue .ejs.
 
 app.set('views', path.join(__dirname, '../views'));
-//But : Indique à Express où trouver tes fichiers de vues.
-//Utilité : Toutes tes pages .ejs doivent être dans ce dossier.
 
 // Middleware pour servir les fichiers statiques (CSS, JS, images)
 app.use(express.static(path.join(__dirname, '../public')));
-//But : Sert les fichiers statiques (CSS, JS, images) depuis le dossier public.
-// Utilité : Permet d’accéder à /style.css ou /newsletter.js dans tes pages.
 // Middleware pour parser les données des formulaires
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -36,8 +25,6 @@ app.use(express.json());
 app.get('/index', (_req, res) => {
     res.render('index', {message : "Welcome les copains bien venu a la page d'accueil"})})
 
-
-// Exemple de route pour la newsletter
 app.get('/newsletter-page', (_req, res) => {
     res.render('newsletter-page', { title: "Envoi de la newsletter" });
 });
