b0fe6a90388a7ee26841230d3bc282e5924d1362
HarryKLM330 <gaetcams@gmail.com>
2025-07-24 03:33:16 +0200

    Correction Zoom creationAtelier

--- public/javascript/adminUserGestion.js
+++
@@ -1,7 +0,0 @@
-let currentFontSize = 1;
-        function increaseFontSize() {
-            currentFontSize++;
-            if (currentFontSize > 4) currentFontSize = 1;
-            document.body.classList.remove('font-size-1', 'font-size-2', 'font-size-3', 'font-size-4');
-            document.body.classList.add('font-size-' + currentFontSize);
-        }
